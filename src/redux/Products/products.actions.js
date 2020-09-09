123489db1eb6c8629e87a4299a0b2584767b2032
Ashley Bibizadeh <ashleybibizadeh@Ashleys-MacBook-Pro.local>
2020-09-09 19:32:36 +0100

    search filters

--- src/redux/Products/products.actions.js
+++ src/redux/Products/products.actions.js
@@ -5,8 +5,9 @@ export const addProductStart = productData => ({
   payload: productData
 });
 
-export const fetchProductsStart = () => ({
-  type: productsTypes.FETCH_PRODUCTS_START
+export const fetchProductsStart = (filters) => ({
+  type: productsTypes.FETCH_PRODUCTS_START,
+  payload: filters
 });
 
 export const setProducts = products => ({
