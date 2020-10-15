884166374555c6306530523df4028312ba444a4f
Ashley Bibizadeh <ashleybibizadeh@Ashleys-MacBook-Pro.local>
2020-10-15 18:32:58 +0100

    persist cart data

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/createStore';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/createStore';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import createSagaMiddle from 'redux-saga';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
@@ -12,4 +13,9 @@ export const middlewares = [thunk, sagaMiddleware, logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {
+  store,
+  persistor
+};
