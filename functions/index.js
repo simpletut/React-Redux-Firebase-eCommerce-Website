7a68660e3de04c0a564ef162a20264412ecf3a2d
Ashley Bibizadeh <ashleybibizadeh@Ashleys-MacBook-Pro.local>
2020-10-31 17:25:43 +0000

    test firebase functions

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({ origion: true }));
+app.use(express.json());
+
+app.get('*', (req, res) => {
+  res
+    .status(404)
+    .send('404, Not Found.');
+});
+
+exports.api = functions.https.onRequest(app);
